ac462f8fe76ae8afacf3de6d5a6f0996e9a737dc
Tomer Aberbach <tomer@aberba.ch>
2025-08-02 19:02:18 -0400

    chore: upgrade all the things

--- test/index.js
+++
@@ -1,8 +0,0 @@
-import { expect, test } from 'vitest'
-
-test(`prettier-config`, async () => {
-  const config = await import(`../src/index.js`)
-
-  expect(config).not.toBeNull()
-  expect(typeof config).toBe(`object`)
-})
